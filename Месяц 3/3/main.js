787603850adae3ddbd54ce0172b2fb9a9adfba9c
Mancerka <116738685+Mancerka@users.noreply.github.com>
2022-11-11 21:58:16 +0600

    Add files via upload

---
+++ Месяц 3/3/main.js
@@ -0,0 +1,40 @@
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr);
+
+
+// let arr = [1,2,3, [4,5,6, [7,8, [9,10], [11,12,13]]]]
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3][0]);
+// console.log(arr[3][1]);
+// console.log(arr[3][2]);
+// console.log(arr[3][3][0]);
+// console.log(arr[3][3][1]);
+// console.log(arr[3][3][3][0]);
+// console.log(arr[3][3][3][1]);
+// console.log(arr[3][3][3][2]);
+
+// let arr = [ [1,2,3], [4,5,6] ]
+// console.log(arr[0][0]);
+// console.log(arr[0][1]);
+// console.log(arr[0][2]);
+// console.log(arr[1][0]);
+// console.log(arr[1][1]);
+// console.log(arr[1][2]);
+
+
+let num1 = +prompt('num1')
+let num2 = +prompt('num2')
+let num3 = +prompt('num3')
+let num4 = +prompt('num4')
+let num5 = +prompt('num5')
+let arr = []
+
+arr.push(num1)
+arr.push(num2)
+arr.push(num3)
+arr.push(num4)
+arr.push(num5)
+
+console.log(arr);
