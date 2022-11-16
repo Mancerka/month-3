9ab46803f173e025cdd8eaaf2800e4bd8b2cc917
Mancerka <116738685+Mancerka@users.noreply.github.com>
2022-11-16 16:48:17 +0600

    4 les

---
+++ Месяц 3/4/main.js
@@ -0,0 +1,47 @@
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+
+// for(let i = 0; i < arr.length; i++){
+//     if (arr[i] === 6) {
+//         console.log(arr);
+//         break;
+//     }
+// }
+
+
+//Задача №1
+// for (let i = 1; i <= 100; i++){
+//     if( i % 2){
+//         console.log(i);
+//     }
+//     else{
+//         document.write(i + '<br>')
+//     }
+// }
+
+
+// Задача #2
+// let arr = [1,2,3,4,5,6,7,8,9]
+// for(let i = 0; i < arr.length; i++){
+//     // console.log(arr[i]);
+//     // console.log(arr[i] * 2);
+//     // console.log(arr[i] ** 3);
+// }
+
+
+//Задача №3
+// let arr = [22,1,5,11,7,12,55,3,9]
+// let state = 0
+// for(let i = 0; i < arr.length; i++){
+//     state += arr[i];
+
+// }
+// console.log(state);
+
+// let arif = state/arr.length
+
+// console.log(arif);
+
