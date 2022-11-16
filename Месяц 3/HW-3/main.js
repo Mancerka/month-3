83f16ca29fb13236a855eb6b1ab0b8a0f89bc51a
Mancerka <116738685+Mancerka@users.noreply.github.com>
2022-11-16 17:19:59 +0600

    home work #3

---
+++ Месяц 3/HW-3/main.js
@@ -0,0 +1,40 @@
+
+
+// Задача №1
+// let arr = [2,5,9,15,0,4]
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 3 && arr[i] < 10){
+//         console.log(arr[i]);
+//     }
+// }
+
+
+//Задача №2
+// let arr = [1,6,-22,33,-50,9,11,25,-5]
+// let res = 0
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         res += arr[i];
+//     }
+// }
+// console.log(res);
+
+
+// //Задача №3
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+// for(let i = 0; i < arr.length; i++){
+//     if(i >= 0){
+//         document.write('-',arr[i]);
+//     }
+// }
+
+
+// Задача №4
+// let line= [];
+// for (let i=1; i < 10; i++) {
+//     line += i;
+//     document.write(line + '<br>');
+// }
